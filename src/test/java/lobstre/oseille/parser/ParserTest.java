a26aa808e95bf075cfcccf7e73f66da502c85959
Florent Le Gall <florent.legall@gmail.com>
2014-11-07 14:00:32 +0100

    Move test assets to fix tests

--- src/test/java/lobstre/oseille/parser/ParserTest.java
+++ src/test/java/lobstre/oseille/parser/ParserTest.java
@@ -13,11 +13,11 @@ import org.junit.Test;
 public class ParserTest {
     @Test
     public void test () throws IOException {
-        Account account = Parser.read (new File ("test/test.txt"));
+        Account account = Parser.read (new File ("test-files/test.txt"));
         Assert.assertEquals (account.getInitialAmount (), new BigDecimal ("300.0"));
         
-        Parser.write (account, new File ("test/test-output.txt"));
-        Account account2 = Parser.read (new File ("test/test-output.txt"));
+        Parser.write (account, new File ("test-files/test-output.txt"));
+        Account account2 = Parser.read (new File ("test-files/test-output.txt"));
         
         Assert.assertEquals (account.getBudgets (), account2.getBudgets ());
         Assert.assertEquals (account.getInitialAmount (), account2.getInitialAmount ());
