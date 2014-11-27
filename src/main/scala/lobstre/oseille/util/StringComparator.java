b7cec3e4d441c7a4fe8453846175b7ac351af7d6
Florent Le Gall <florent.legall@gmail.com>
2014-11-27 13:42:35 +0100

    Clean up unused code

--- src/main/scala/lobstre/oseille/util/StringComparator.java
+++ src/main/scala/lobstre/oseille/util/StringComparator.java
@@ -8,28 +8,16 @@ public class StringComparator implements Comparator<String> {
 	}
 	
 	public static int compareStrings(String o1, String o2) {
-		return comp(o1, o2, true);
-	}
-	
-	public static int compareStringsIgnoreCase(String o1, String o2) {
-		return comp(o1, o2, false);
-	}
-
-	private static int comp(String o1, String o2, boolean caseSensitive) {
-		if (o1 == null) {
-			if (o2 == null) {
-				return 0;
-			} else {
-				return -1;
-			}
-		}
-		if (o2 == null) {
-			return 1;
-		}
-		if (caseSensitive) {
-			return o1.compareTo(o2);
-		} else {
-			return o1.compareToIgnoreCase(o2);
-		}
-	}
+        if (o1 == null) {
+            if (o2 == null) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+        if (o2 == null) {
+            return 1;
+        }
+        return o1.compareTo(o2);
+    }
 }
