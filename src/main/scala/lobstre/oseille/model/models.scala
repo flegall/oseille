82a84473bdcc9dc71c459b8bc72d1bb44043aafc
Florent Le Gall <florent.legall@gmail.com>
2015-04-12 23:11:06 +0200

    Add scala model classes

---
+++ src/main/scala/lobstre/oseille/model/models.scala
@@ -0,0 +1,23 @@
+package lobstre.oseille.model
+
+sealed trait Expense {
+  def category: String
+  def label: String
+  def amount: BigDecimal
+}
+
+case class Damocles(category: String, label: String, amount: BigDecimal)
+  extends Expense
+
+case class Prevision(category: String, label: String, amount: BigDecimal)
+  extends Expense
+
+case class Operation(category: String, label: String, amount: BigDecimal, date: String)
+  extends Expense
+
+case class Account(initialAmount: BigDecimal,
+                   budgets: Map[String, BigDecimal],
+                   damocleses: Seq[Damocles],
+                   previsions: Seq[Damocles],
+                   operations: Seq[Operation])
+
