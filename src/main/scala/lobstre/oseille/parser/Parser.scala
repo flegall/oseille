5d767e7f66359ba19e8e920357c6d46ec80a9725
Florent Le Gall <florent.legall@gmail.com>
2015-04-13 12:05:20 +0200

    Rewrote Parser in scala

---
+++ src/main/scala/lobstre/oseille/parser/Parser.scala
@@ -0,0 +1,126 @@
+package lobstre.oseille.parser
+
+import java.io._
+import java.math.BigDecimal
+
+import lobstre.oseille.model.{MutableOperation, MutableDamocles, MutablePrevision, MutableAccount}
+import lobstre.oseille.util.Util
+
+object Parser {
+  private val INIT_TOKEN: String = "init:"
+  private val BUDGET_TOKEN: String = "budget:"
+  private val PREVISION_TOKEN: String = "prevision:"
+  private val DAMOCLES_TOKEN: String = "damocles:"
+  private val OPERATION_TOKEN: String = "operation:"
+
+  @throws(classOf[IOException])
+  def read(file: File): MutableAccount = {
+    val acc: MutableAccount = new MutableAccount
+    val in: BufferedReader = new BufferedReader(new FileReader(file))
+    try {
+      var line: String = null
+      while (null != {
+        line = in.readLine
+        line
+      }) {
+        val split: Array[String] = line.split(":")
+        if (line.startsWith(INIT_TOKEN)) {
+          check(INIT_TOKEN, split, 2)
+          acc.setInitialAmount(Util.getBD(split(1)))
+        }
+        if (line.startsWith(BUDGET_TOKEN)) {
+          check(BUDGET_TOKEN, split, 3)
+          val bd: BigDecimal = Util.getBD(split(2))
+          acc.getBudgets.put(split(1), bd)
+        }
+        if (line.startsWith(PREVISION_TOKEN)) {
+          check(PREVISION_TOKEN, split, 4)
+          val p: MutablePrevision = new MutablePrevision
+          p.setCategory(split(1))
+          p.setLabel(split(2))
+          p.setAmount(Util.getBD(split(3)))
+          acc.getPrevisions.add(p)
+        }
+        if (line.startsWith(DAMOCLES_TOKEN)) {
+          check(DAMOCLES_TOKEN, split, 4)
+          val dc: MutableDamocles = new MutableDamocles
+          dc.setCategory(split(1))
+          dc.setLabel(split(2))
+          dc.setAmount(Util.getBD(split(3)))
+          acc.getDamocleses.add(dc)
+        }
+        if (line.startsWith(OPERATION_TOKEN)) {
+          check(OPERATION_TOKEN, split, 5)
+          val o: MutableOperation = new MutableOperation
+          o.setCategory(split(1))
+          o.setLabel(split(2))
+          o.setAmount(Util.getBD(split(3)))
+          o.setDate(split(4))
+          acc.getOperations.add(o)
+        }
+      }
+    } finally {
+      in.close()
+    }
+    initZeroBudgets(acc)
+    acc.sortOperations
+    acc
+  }
+
+  @throws(classOf[IOException])
+  def write(acc: MutableAccount, file: File) {
+    acc.sortOperations
+    initZeroBudgets(acc)
+    val pw: PrintWriter = new PrintWriter(file)
+    try {
+      pw.println(INIT_TOKEN + acc.getInitialAmount.toString)
+      import scala.collection.JavaConversions._
+      for (b <- acc.getBudgets.entrySet) {
+        pw.println(BUDGET_TOKEN + b.getKey + ":" + b.getValue)
+      }
+      import scala.collection.JavaConversions._
+      for (p <- acc.getPrevisions) {
+        pw.println(PREVISION_TOKEN + p.getCategory + ":" + p.getLabel + ":" + p.getAmount)
+      }
+      import scala.collection.JavaConversions._
+      for (d <- acc.getDamocleses) {
+        pw.println(DAMOCLES_TOKEN + d.getCategory + ":" + d.getLabel + ":" + d.getAmount)
+      }
+      import scala.collection.JavaConversions._
+      for (o <- acc.getOperations) {
+        pw.println(OPERATION_TOKEN + o.getCategory + ":" + o.getLabel + ":" + o.getAmount + ":" + o.getDate)
+      }
+    } finally {
+      pw.close()
+    }
+  }
+
+  private def initZeroBudgets(acc: MutableAccount) {
+    import scala.collection.JavaConversions._
+    for (p <- acc.getPrevisions) {
+      if (!acc.getBudgets.containsKey(p.getCategory)) {
+        acc.getBudgets.put(p.getCategory, BigDecimal.valueOf(0))
+      }
+    }
+    import scala.collection.JavaConversions._
+    for (d <- acc.getDamocleses) {
+      if (!acc.getBudgets.containsKey(d.getCategory)) {
+        acc.getBudgets.put(d.getCategory, BigDecimal.valueOf(0))
+      }
+    }
+    import scala.collection.JavaConversions._
+    for (o <- acc.getOperations) {
+      if (!acc.getBudgets.containsKey(o.getCategory)) {
+        acc.getBudgets.put(o.getCategory, BigDecimal.valueOf(0))
+      }
+    }
+  }
+
+  private def check(initToken: String, split: Array[String], expected: Int) {
+    if (split.length != expected) {
+      throw new RuntimeException(initToken + " expected " + expected + " values")
+    }
+  }
+}
+
+
--- src/test/scala/lobstre/oseille/parser/Parser_Spec.scala
+++ src/test/scala/lobstre/oseille/parser/Parser_Spec.scala
@@ -3,7 +3,9 @@ package lobstre.oseille.parser
 import java.io.File
 import java.math.BigDecimal
 
+import lobstre.oseille.model.MutableDamocles
 import lobstre.oseille.parser.Parser.{read, write}
+import lobstre.oseille.util.Util
 import org.scalatest.{FlatSpec, Matchers}
 
 class Parser_Spec extends FlatSpec with Matchers {
@@ -12,7 +14,27 @@ class Parser_Spec extends FlatSpec with Matchers {
   it should "parse accounts correctly" in {
     val account = read(new File("test-files/test.txt"))
 
-    account.getInitialAmount shouldBe new BigDecimal("300.0")
+    account.getInitialAmount shouldBe Util.getBD("300.0")
+    account.getBudgets.get("car") shouldBe Util.getBD("600")
+
+    account.getDamocleses should have size 1
+    val damocles = account.getDamocleses.get(0)
+    damocles.getCategory shouldBe "car"
+    damocles.getLabel shouldBe "plein-essence"
+    damocles.getAmount shouldBe Util.getBD("55.75")
+
+    account.getPrevisions should have size 2
+    val prevision = account.getPrevisions.get(0)
+    prevision.getCategory shouldBe "car"
+    prevision.getLabel shouldBe "vidange"
+    prevision.getAmount shouldBe Util.getBD("25.75")
+
+    account.getOperations should have size 3
+    val operation = account.getOperations.get(0)
+    operation.getCategory shouldBe "car"
+    operation.getLabel shouldBe "plein-essence"
+    operation.getAmount shouldBe Util.getBD("45.75")
+    operation.getDate shouldBe "10-08-10"
   }
 
   it should "write accounts correctly" in {
